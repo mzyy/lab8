fe75bbf87df0e94defdb6ff1b8f421ea7bf00f5b
Mengyuan Zhang <m4zhang@ucsd.edu>
2021-02-25 10:48:02 -0800

    lab8

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,10 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'ARad5DfLVMQp03UULsQ4nhY9fAuFXuPy';
+
+// 'map' refers to a <div> element with the ID map
+L.mapquest.map('map', {
+  center: [37.7749, -122.4194],
+  layers: L.mapquest.tileLayer('map'),
+  zoom: 12
+});
 }
